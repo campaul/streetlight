10631132056801ae60737c8062f39a49771a5b27
Cameron Paul <cpaul37@gmail.com>
2023-07-07 19:59:26 -0500

    Add status_checker example

---
+++ examples/status_checker.rs
@@ -0,0 +1,24 @@
+use std::env;
+use std::{io::Cursor, net::TcpStream};
+
+use streetlight::{read_response, write_request, Request};
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let url = &args[1];
+
+    let mut stream = TcpStream::connect(url)?;
+
+    let request = Request::builder()
+        .uri("/")
+        .body(Cursor::new(vec![]))
+        .unwrap();
+
+    write_request(&mut stream, request)?;
+
+    let response = read_response(&mut stream)?;
+
+    println!("Status code: {}", response.status());
+
+    Ok(())
+}
